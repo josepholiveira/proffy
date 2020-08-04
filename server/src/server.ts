3156cb13ee0ea052f3b223ed9c1fc0825c3fbbce
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-08-04 09:46:59 -0300

    Dia 02: Olhando as oportunidades

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import routes from './routes';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+app.use(routes);
+
+
+app.listen(3333, () => {
+  console.log('Listening on port 3333')
+})
