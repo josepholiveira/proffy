ee154afd29b3832eaaac04f127ac1dd51aad2487
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-08-06 11:34:33 -0300

    Dia 04: Ate 2 anos em 2 meses

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, ImageBackground, Text } from 'react-native';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png'
+
+import styles from './styles';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+const GiveClasses: React.FC = () => {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack()
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground 
+        resizeMode="contain" 
+        source={giveClassesBgImage} 
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>Para começar, você precisa se cadastrar como professor na nossa plataforma web.</Text>
+      </ImageBackground>
+      <RectButton style={styles.okButton} onPress={() => handleNavigateBack()}>
+        <Text style={styles.okButtonText}>Tudo bem!</Text>
+      </RectButton>
+    </View>
+  );
+}
+
+export default GiveClasses;
